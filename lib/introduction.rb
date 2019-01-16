bb65668d9bad958b6f7c3a7ccf8b52bcf3356fee
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-01-16 19:43:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,5 +2,5 @@ name = "Josh"
 language = "Ruby"
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name.strip} and I am learning to program in #{language}."
 end
